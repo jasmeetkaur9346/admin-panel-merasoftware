8d4978d99a95ebf976964f3594e0076e0ce9ce26
jasmeetkaur9346 <vacomputers.com@gmail.com>
2025-10-18 13:44:06 +0530

    admin panel added

--- src/App.js
+++ src/App.js
@@ -1,36 +1,36 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import TriangleMazeLoader from "./components/TriangleMazeLoader";
-import { useAuth } from "./context/AuthContext";
-import { MAIN_WEBSITE_URL } from "./common";
+import Header from './components/Header';
+import Footer from './components/Footer';
+import TriangleMazeLoader from './components/TriangleMazeLoader';
+import { useAuth } from './context/AuthContext';
+import { MAIN_WEBSITE_URL } from './common';
 
-import AdminPanel from "./pages/AdminPanel";
-import AdminDashboard from "./pages/AdminDashboard";
-import AdminManagement from "./pages/AdminManagement";
-import ManagerManagement from "./pages/ManagerManagement";
-import DeveloperManagement from "./pages/DeveloperManagement";
-import PartnerManagement from "./pages/PartnerManagement";
-import CustomerManagement from "./pages/CustomerManagement";
-import AdminFileSettings from "./pages/AdminFileSettings";
-import AdminProjects from "./pages/AdminProjects";
-import AdminUpdateRequests from "./pages/AdminUpdateRequests";
-import AdminWithdrawalManagement from "./pages/AdminWithdrawalManagement";
-import AdminPaymentVerification from "./pages/AdminPaymentVerification";
-import AdminCouponPage from "./pages/AdminCouponPage";
-import AdminTicketsDashboard from "./pages/AdminTicketsDashboard";
-import PendingRenewals from "./pages/PendingRenewals";
-import KYCVerification from "./pages/KYCVerification";
-import AllProducts from "./pages/AllProducts";
-import AllCategory from "./pages/AllCategory";
-import AllAds from "./pages/AllAds";
-import AllDevelopers from "./pages/AllDevelopers";
-import AllWelcomeContent from "./pages/AllWelcomeContent";
-import WalletManagement from "./pages/WalletManagement";
+import AdminPanel from './pages/AdminPanel';
+import AdminDashboard from './pages/AdminDashboard';
+import AdminManagement from './pages/AdminManagement';
+import ManagerManagement from './pages/ManagerManagement';
+import DeveloperManagement from './pages/DeveloperManagement';
+import PartnerManagement from './pages/PartnerManagement';
+import CustomerManagement from './pages/CustomerManagement';
+import AdminFileSettings from './pages/AdminFileSettings';
+import AdminProjects from './pages/AdminProjects';
+import AdminUpdateRequests from './pages/AdminUpdateRequests';
+import AdminWithdrawalManagement from './pages/AdminWithdrawalManagement';
+import AdminPaymentVerification from './pages/AdminPaymentVerification';
+import AdminCouponPage from './pages/AdminCouponPage';
+import AdminTicketsDashboard from './pages/AdminTicketsDashboard';
+import PendingRenewals from './pages/PendingRenewals';
+import KYCVerification from './pages/KYCVerification';
+import AllProducts from './pages/AllProducts';
+import AllCategory from './pages/AllCategory';
+import AllAds from './pages/AllAds';
+import AllDevelopers from './pages/AllDevelopers';
+import AllWelcomeContent from './pages/AllWelcomeContent';
+import WalletManagement from './pages/WalletManagement';
 
 function App() {
   const { user, initialising } = useAuth();
@@ -68,8 +68,7 @@ function App() {
         <Header />
         <main className="min-h-[calc(100vh-160px)]">
           <Routes>
-            <Route path="/" element={<Navigate to="/admin-panel" replace />} />
-            <Route path="/admin-panel/*" element={<AdminPanel />}>
+            <Route path="/" element={<AdminPanel />}>
               <Route index element={<AdminDashboard />} />
               <Route path="dashboard" element={<AdminDashboard />} />
               <Route path="admins" element={<AdminManagement />} />
@@ -93,7 +92,7 @@ function App() {
               <Route path="wallet-management" element={<WalletManagement />} />
               <Route path="partner-withdrawal-requests" element={<AdminWithdrawalManagement />} />
             </Route>
-            <Route path="*" element={<Navigate to="/admin-panel" replace />} />
+            <Route path="*" element={<Navigate to="/dashboard" replace />} />
           </Routes>
         </main>
         <Footer />
